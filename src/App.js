b39b5bc5425e02054bd13b21d9baccccd9dbecbb
Melody Chung <Melody@MelodydeMacBook-Pro.local>
2022-11-14 23:17:28 -0500

    incoming change

--- src/App.js
+++ src/App.js
@@ -6,31 +6,6 @@ import Home from './pages/Home';
 function App() {
 
   return (
-<<<<<<< HEAD
-    
-    <>
-    <div className="header">
-      <Header />
-    </div>
-    <main>
-        <div className='video__container'>
-          <video className='hero-video' controls poster={currentVideo.image}>
-          <source src={currentVideo.video} type=""/>
-          <source src={currentVideo.video} type=""/>
-          </video>
-        </div>
-        <section className='bottom-container'>
-            <div className='main-left'> 
-              <MainVideoInfo
-              currentVideo={currentVideo}
-              /> 
-              <Comments 
-                currentVideo={currentVideo}
-                />
-   
-            </div>
-=======
->>>>>>> upload-page
 
     <BrowserRouter>
     <Routes>
